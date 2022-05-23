d8bea2f4caa1b549a3a22c94831b3a8cc777272c
Michael Su <mailmsuu@gmail.com>
2022-05-23 10:31:56 +0100

    added and passed test for statement when passing through an array of transactions

--- statement.js
+++ statement.js
@@ -3,11 +3,13 @@ class Statement {
     this.header = "date || credit || debit || balance"
   }
 
-  print () {
-    console.log(this.header);
+  print (history = []) {
+    let statement = this.header;
+    history.forEach((transaction) => {
+      statement += `\n${transaction.date} || ${transaction.credit}|| ${transaction.debit}|| ${transaction.balance}`;
+    })
+    console.log(statement);
   }
-
-
 }
 
 module.exports = Statement;
--- statement.test.js
+++ statement.test.js
@@ -3,6 +3,19 @@ const Statement = require('./statement');
 describe('Statement', () => {
   beforeEach(() => {
     statement = new Statement();
+    mockTransaction = {
+      date: "22/05/2022",
+      credit: "500.00 ",
+      debit: "",
+      balance: "500.00",
+    };
+    mockTransaction2 = {
+      date: "22/05/2022",
+      credit: "",
+      debit: "200.00 ",
+      balance: "300.00",
+    };
+    array = [mockTransaction2, mockTransaction];
   })
 
   it('should print only the header when there are no transactions', () => {
@@ -10,6 +23,16 @@ describe('Statement', () => {
     statement.print();
     expect(console.log).toHaveBeenCalledWith(
       "date || credit || debit || balance"
+    );
+  })
+
+  it('should print out transactions in the bank statement formatted to 2 decimal places', () => {
+    console.log = jest.fn();
+    statement.print(array);
+    expect(console.log).toHaveBeenCalledWith(
+      "date || credit || debit || balance" +
+        "\n22/05/2022 || || 200.00 || 300.00" +
+        "\n22/05/2022 || 500.00 || || 500.00"
     );
   })
 })
