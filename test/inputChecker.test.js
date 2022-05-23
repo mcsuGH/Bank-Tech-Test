4d9b3ed8635dab49b76153630e8f9a51d492be4c
Michael Su <mailmsuu@gmail.com>
2022-05-23 15:45:37 +0100

    added and passed test to check if number is negative in inputchecker

--- test/inputChecker.test.js
+++ test/inputChecker.test.js
@@ -13,7 +13,13 @@ describe('InputChecker', () => {
 
   it('raises an error if the number has too many decimals', () => {
     expect(() => {
-      checker.checkNumber('1.234');
+      checker.checkNumber(1.234);
+    }).toThrowError('Please enter a valid amount');
+  });
+
+  it('raises an error if the number is negative', () => {
+    expect(() => {
+      checker.checkNumber(-1);
     }).toThrowError('Please enter a valid amount');
   });
 })
