152249980be2cb32281640755ee803564a99c9a7
Michael Su <mailmsuu@gmail.com>
2022-05-23 15:47:39 +0100

    added and passed test for balance check in inputchecker

--- test/inputChecker.test.js
+++ test/inputChecker.test.js
@@ -5,21 +5,32 @@ describe('InputChecker', () => {
     checker = new InputChecker()
   })
 
-  it('raises an error if the number is not a number', () => {
-    expect(() => {
-      checker.checkNumber('bob');
-    }).toThrowError('Please enter a valid amount');
-  });
+  describe('checkNumber', () => {
+    it('raises an error if the number is not a number', () => {
+      expect(() => {
+        checker.checkNumber('bob');
+      }).toThrowError('Please enter a valid amount');
+    });
+  
+    it('raises an error if the number has too many decimals', () => {
+      expect(() => {
+        checker.checkNumber(1.234);
+      }).toThrowError('Please enter a valid amount');
+    });
+  
+    it('raises an error if the number is negative', () => {
+      expect(() => {
+        checker.checkNumber(-1);
+      }).toThrowError('Please enter a valid amount');
+    });
+  })
 
-  it('raises an error if the number has too many decimals', () => {
-    expect(() => {
-      checker.checkNumber(1.234);
-    }).toThrowError('Please enter a valid amount');
-  });
+  describe('checkBalance', () => {
+    it('raises an error if number exceeds balance', () => {
+      expect(() => {
+        checker.checkBalance(101, 100);
+      }).toThrowError('Insufficient balance');
+    })
+  })
 
-  it('raises an error if the number is negative', () => {
-    expect(() => {
-      checker.checkNumber(-1);
-    }).toThrowError('Please enter a valid amount');
-  });
 })
