0c41a60801cd2f99258c126469b09734526a350b
Michael Su <mailmsuu@gmail.com>
2022-05-23 18:05:15 +0100

    added and passed test for updating history after a deposit

--- test/transactionHistory.test.js
+++ test/transactionHistory.test.js
@@ -1,14 +1,23 @@
 const TransactionHistory = require('../lib/transactionHistory');
+const Transaction = require('../lib/transaction');
+
+jest.mock('../lib/transaction');
 
 describe('TransactionHistory', () => {
   beforeEach(() => {
-    history = new TransactionHistory();
+    history = new TransactionHistory(Transaction);
   })
 
   describe('history', () => {
     it('should have an empty history to begin with', () => {
-      expect(history.history).toEqual([]);
+      expect(history.log).toEqual([]);
     });
+
+    it('transaction history should be updated after a deposit', () => {
+      history.recordDeposit(500);
+      expect(history.log.length).toEqual(1);
+    });
+    
   });
 
 })
